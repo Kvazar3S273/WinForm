626990825319a7b724824e5d25bb2b5613e50e4d
Kvazar3S273 <ba2h007@gmail.com>
2021-01-19 17:46:39 +0200

    create

---
+++ PhoneBook/PhoneBook/PhoneBook/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PhoneBook
+{
+    [Table("tblHuman")]
+    public class Human
+    {
+        [Key]
+        public int Id { get; set; }
+    
+        [Required, StringLength(100)]
+        public string Surname { get; set; }
+    
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+    
+        [Required, StringLength(15)]
+        public string Phone { get; set; }
+        
+        [Required, StringLength(10)]
+        public string Sex { get; set; }
+    }
+}
---
+++ PhoneBook/PhoneBook/PhoneBook/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook
+{
+    class MyContext : DbContext
+    {
+        public DbSet<Human> Humans { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=95.214.10.36;Port=5432;Database=ba2hdb;Username=ba2h;Password=$544$B77w**G)K$t!ba2h22}");
+        }
+    }
+}
