a807c84eecd4d73a13a109116085deb18bea6578
Kvazar3S273 <ba2h007@gmail.com>
2021-02-28 22:39:03 +0200

    get List filters

--- Rozetka/Rozetka/MainForm.cs
+++ Rozetka/Rozetka/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using Rozetka.Entities;
+using Rozetka.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,6 +20,50 @@ namespace Rozetka
             InitializeComponent();
             _context = new EFContext();
             Seeder.SeedDatabase(_context);
+            var filters = GetListFilters();
+        }
+        private List<FNameViewModel> GetListFilters()
+        {
+            var queryName = from f in _context.FilterNames.AsQueryable()
+                            select f;
+            var queryGroup = from g in _context.FilterNameGroups.AsQueryable()
+                             select g;
+            //Отримуємо загальну множину значень
+            var query = from u in queryName
+                        join g in queryGroup on u.Id equals g.FilterNameId into ua
+                        from aEmp in ua.DefaultIfEmpty()
+                        select new
+                        {
+                            FNameId = u.Id,
+                            FName = u.Name,
+                            FValueId = aEmp != null ? aEmp.FilterValueId : 0,
+                            FValue = aEmp != null ? aEmp.FilterValueOf.Name : null,
+                        };
+
+
+            //Групуємо по іменам і сортуємо по спаданню імен
+
+            var groupNames = query.AsEnumerable()
+                      .GroupBy(f => new { Id = f.FNameId, Name = f.FName })
+                      .Select(g => g)
+                      .OrderByDescending(p => p.Key.Name);
+
+            //По групах отримуємо
+            var result = from fName in groupNames
+                         select
+                         new FNameViewModel
+                         {
+                             Id = fName.Key.Id,
+                             Name = fName.Key.Name,
+                             Children = fName.Select(x =>
+                                   new FValueViewModel
+                                   {
+                                       Id = x.FValueId,
+                                       Value = x.FValue
+                                   }).OrderBy(l => l.Value).ToList()
+                         };
+
+            return result.ToList();
         }
 
     }
---
+++ Rozetka/Rozetka/Models/FilterViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rozetka.Models
+{
+    public class FValueViewModel
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public bool IsChecked { get; set; } = false;
+    }
+    public class FNameViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCollapsed { get; set; } = true;
+        public List<FValueViewModel> Children { get; set; }
+    }
+}
